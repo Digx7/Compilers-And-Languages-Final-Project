2b552821a3fa52fd6b7a1be498eb12e37accd319
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-05-13 11:55:41 -0700

    Presented Project

---
+++ out.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int p1 , p2q , pr ; 
+p1 = 33 ; 
+p2q = 412 ; 
+pr = p1 + p2q ; 
+cout << pr << endl;
+pr = p1 * ( p2q + 2 * pr ) ; 
+cout << "value=" << pr << endl;
+cout << pr << endl;
+return 0;
+}
